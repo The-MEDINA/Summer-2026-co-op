a1ab5de6560213916c2d94c5809210aafb9b7680
jcf8537 <jacobfoltz428@gmail.com>
2026-05-12 12:18:26 -0400

    Some work on Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private int health;
+    private int energy;
+    private List<CardParent> deck = new List<CardParent>();
+    private List<CardParent> hand = new List<CardParent>();
+    private List<CardParent> inPlay = new List<CardParent>();
+    private List<CardParent> discard = new List<CardParent>();
+    private CardParent commander;
+    private float timer;
+    [SerializeField] private int timeForEnergy = 5;
 
-    // Update is called once per frame
+    public int Health {  get { return health; } set { health = value; } }
+    public int Energy { get { return energy; } set { energy = value; } }
+
     void Update()
     {
-        
+        if (timer > timeForEnergy)
+        {
+            Energy++;
+            timer = 0;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    //BeginGame()
+
+    //PlayCard()?
+
+    //DrawCard()
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if(health < 0) { Death(); }
+    }
+
+    public void Death()
+    {
+
     }
 }
