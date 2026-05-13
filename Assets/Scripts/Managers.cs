6ae03870d1b36461d491b810e57e3450735d82e6
BIK333 <brandk0313@gmail.com>
2026-05-13 12:04:26 -0400

    What I currently got

--- Assets/Scripts/Managers.cs
+++ Assets/Scripts/Managers.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class CardSelectionManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static CardSelectionManager Instance;
+
+    public CardParent selectedCard;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SelectCard(CardParent card)
     {
-        
+        if (selectedCard == card)
+        {
+            ActivateCard(card);
+            return;
+        }
+
+        selectedCard = card;
+
+        Debug.Log(card.name + " selected");
+    }
+
+    public void ActivateCard(CardParent card)
+    {
+        Debug.Log(card.name + " activated");
+
+        selectedCard = null;
+    }
+
+    public void ClearSelection()
+    {
+        selectedCard = null;
     }
 }
