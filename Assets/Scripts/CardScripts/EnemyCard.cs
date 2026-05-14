5e7585870e67a8c4ae0d429e79555201121f2acd
jcf8537 <jacobfoltz428@gmail.com>
2026-05-14 14:05:18 -0400

    comment

--- Assets/Scripts/CardScripts/EnemyCard.cs
+++ Assets/Scripts/CardScripts/EnemyCard.cs
@@ -4,6 +4,11 @@ public class EnemyCard : MonoBehaviour
 {
     private CardClickHandler thisClickHandler;
 
+    //this script exists to test out interactions against opposing cards
+    //because card creation is currently handled in battleground, and will eventually be moved to player, enemy cards need a player of their own
+    //they'll get one eventually, both in an ai player and in the opposing player from the network
+    //but for now this is a simple patch fix - Jake
+
     void Start()
     {
         thisClickHandler = GetComponent<CardClickHandler>();
