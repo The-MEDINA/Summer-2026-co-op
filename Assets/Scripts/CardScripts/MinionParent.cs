9cee78b038491c1ae963f0bd25d265c220e7b82f
jcf8537 <jacobfoltz428@gmail.com>
2026-05-21 10:01:59 -0400

    minionparent started not finished

---
+++ Assets/Scripts/CardScripts/MinionParent.cs
@@ -0,0 +1,89 @@
+using cardIndex;
+using UnityEngine;
+
+public class MinionParent : NewVirtualCardParent
+{
+    public enum effect
+    {
+        none,
+        deathtouch, //just works off base damage for right now, probably want to change this
+        explode,
+        haste,
+        sloth
+    }
+
+    private int health;
+    private int damage;
+    private effect cardEffect;
+    private bool isDead = false;
+    [SerializeField] private bool canAttack = false;
+
+    public int Health { get { return health; } set { health = value; } }
+    public int Damage { get { return damage; } set { damage = value; } }
+    public bool IsDead { get { return isDead; } }
+    public effect CardEffect { get { return cardEffect; } }
+    public bool CanAttack { get { return canAttack; } set { canAttack = value; } }
+
+    public MinionParent(int cost, int health, int damage, string name, type cardType, effect cardEffect, location cardLocation) 
+        : base(cost, name, cardType, cardLocation)
+    {
+        this.health = health;
+        this.damage = damage;
+        this.cardEffect = cardEffect;
+    }
+
+    /// <summary>
+    /// Construct a card using only its name. It should be noted that this constructor will set any int value that's not defined as -1.
+    /// </summary>
+    /// <param name="name">Name of the card.</param>
+    /// <param name="cardLocation">location of the card.</param>
+    public MinionParent(string name, location cardLocation) : base(name, cardLocation)
+    {
+        Details cardDetails = cardIndex.Index.GetDetails(name);
+        health = cardDetails.health;
+        damage = cardDetails.damage;
+        cardEffect = cardDetails.ability;
+    }
+
+    public override void OnPlay()
+    {
+        Debug.Log("a");
+    }
+    public void Attack(MinionParent target)
+    {
+        if (canAttack)
+        {
+            if (target == null || isDead)
+            {
+                return;
+            }
+            if (cardEffect == effect.deathtouch)
+            {
+                target.TakeDamage(this, 99999999);
+            }
+            else
+            {
+                target.TakeDamage(this, Damage);
+            }
+            canAttack = false;
+        }
+    }
+
+    public void TakeDamage(MinionParent attacker, int damage)
+    {
+        Health -= damage;
+
+        if (Health <= 0)
+        {
+            Health = 0;
+            if (cardEffect == effect.explode) { attacker.TakeDamage(this, Damage); }
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        CardLocation = location.discard;
+    }
+}
