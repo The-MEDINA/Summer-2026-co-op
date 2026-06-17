e9a3c9e5ba409fd4952f9ad6ce19db5d94ae4c69
jcf8537 <jacobfoltz428@gmail.com>
2026-06-16 20:24:37 -0400

    missing some implementation for deckbuilder but very good rn

--- Assets/Scripts/DeckbuildScripts/DBButtonScript.cs
+++ Assets/Scripts/DeckbuildScripts/DBButtonScript.cs
@@ -1,14 +1,29 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
+using System;
 
 public class DBButtonScript : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private string cardName = "Cat";
     [SerializeField] private bool isAdding = true;
+    [SerializeField] private TMP_Text numCardsText;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (isAdding) { DeckInstanceDeckbuilderScript.instance.AddCard(cardName); }
-        else { DeckInstanceDeckbuilderScript.instance.RemoveCard(cardName); }
+        if (isAdding) 
+        {
+            DeckInstanceDeckbuilderScript.instance.AddCard(cardName);
+            int currentNum = Convert.ToInt32(numCardsText.text);
+            currentNum++;
+            numCardsText.text = currentNum.ToString();
+        }
+        else 
+        {
+            DeckInstanceDeckbuilderScript.instance.RemoveCard(cardName);
+            int currentNum = Convert.ToInt32(numCardsText.text);
+            currentNum--;
+            numCardsText.text = currentNum.ToString();
+        }
     }
 }
