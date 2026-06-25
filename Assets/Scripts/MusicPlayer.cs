98834165f4a4f15c3118042a10b068bbf5f5e79d
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2026-06-25 09:59:03 -0600

    Tiny changes + docs cause my code was actually fine

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -7,65 +7,108 @@ public class MusicPlayer : MonoBehaviour
         player1lead,
         transition1to2,
         player2lead,
-        transition2to1
+        transition2to1,
+        noLoop
     }
 
+    /// <summary>
+    /// Beginning is optional.
+    /// </summary>
     [SerializeField] private AudioClip beginning;
+    /// <summary>
+    /// This should be the looping part of the music, repeated TWICE.
+    /// This is so that the music player can fade it in and out at the middle, to prevent any gaps in the sound.
+    /// </summary>
     [SerializeField] private AudioClip loopx2;
+
     [Header("These should be two separate audio sources.")] 
     [SerializeField] private AudioSource musicPlayer1;
     [SerializeField] private AudioSource musicPlayer2;
+    
     [Header("")]
     [Range(0.0f, 1.0f)]
     [SerializeField] private float volume;
-    [SerializeField] private float transitionTime = 5;
+
+    /// <summary>
+    /// Short transition time recommended. Long ones make the transition too obvious.
+    /// </summary>
+    [Tooltip("Short transition time recommended. Long ones make the transition too obvious.")]
+    [SerializeField] private float transitionTime = 0.2f;
+
+    [Tooltip("change this if there's a gap between the beginning and loop or if they clash at some point.")]
     [SerializeField] private float offset = -0.075f;
 
     private state current = state.player2lead;
     private float deltaVolume = 0;
-    private bool beginningPlayed = false;
-    private double time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         deltaVolume = volume / transitionTime;
         musicPlayer1.volume = volume;
         musicPlayer2.volume = volume;
+        if (loopx2 != null) musicPlayer2.clip = loopx2;
+        else current = state.noLoop;
 
+        // play the beginning first if there is one.
         if (beginning != null)
         {
             musicPlayer1.clip = beginning;
+            musicPlayer1.Play();
+            musicPlayer2.PlayDelayed(beginning.length + offset);
         }
         else
         {
             musicPlayer1.clip = loopx2;
-            beginningPlayed = true;
-        } 
-        musicPlayer2.clip = loopx2;
-        musicPlayer1.Play();
-        musicPlayer2.PlayDelayed(beginning.length + offset);
+            musicPlayer2.Play();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (musicPlayer2.time >= (loopx2.length / 2) && current == state.player2lead)
+        if (current != state.noLoop)
         {
-            current = state.transition2to1;
-            musicPlayer1.clip = loopx2;
-            musicPlayer1.time = 0;
-            musicPlayer1.volume = 0;
-            musicPlayer1.Play();
-        }
-        else if (current == state.transition2to1)
-        {
-            musicPlayer1.volume += Time.deltaTime * deltaVolume;
-            musicPlayer2.volume -= Time.deltaTime * deltaVolume;
-            if (musicPlayer2.volume <= 0 || musicPlayer1.volume >= volume)
+            // start fading at the halfway point.
+            if (musicPlayer2.time >= (loopx2.length / 2) && current == state.player2lead)
             {
-                current = state.player1lead;
-                musicPlayer1.volume = volume;
+                current = state.transition2to1;
+                musicPlayer1.clip = loopx2;
+                musicPlayer1.time = 0;
+                musicPlayer1.volume = 0;
+                musicPlayer1.Play();
+            }
+            // fade here
+            else if (current == state.transition2to1)
+            {
+                musicPlayer1.volume += Time.deltaTime * deltaVolume;
+                musicPlayer2.volume -= Time.deltaTime * deltaVolume;
+                if (musicPlayer2.volume <= 0 || musicPlayer1.volume >= volume)
+                {
+                    current = state.player1lead;
+                    musicPlayer1.volume = volume;
+                    musicPlayer2.volume = 0;
+                }
+            }
+            // start fading at the halfway point
+            else if (musicPlayer1.time >= (loopx2.length / 2) && current == state.player1lead)
+            {
+                current = state.transition1to2;
+                musicPlayer2.clip = loopx2;
+                musicPlayer2.time = 0;
                 musicPlayer2.volume = 0;
+                musicPlayer2.Play();
+            }
+            // more fading here
+            else if (current == state.transition1to2)
+            {
+                musicPlayer2.volume += Time.deltaTime * deltaVolume;
+                musicPlayer1.volume -= Time.deltaTime * deltaVolume;
+                if (musicPlayer1.volume <= 0 || musicPlayer2.volume >= volume)
+                {
+                    current = state.player2lead;
+                    musicPlayer2.volume = volume;
+                    musicPlayer1.volume = 0;
+                }
             }
         }
     }
