d084ee05678ff194dfa809dfedc26ddd19f9e676
BIK333 <brandk0313@gmail.com>
2026-06-26 05:25:08 -0400

    Brightness slider implementation

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void LoadLocalDemo()
+    {
+        SceneManager.LoadScene("Demo_LocalTwoPlayer");
+    }
+
+    public void LoadLanDemo()
+    {
+        SceneManager.LoadScene("DirectConnectTest");
+    }
+}
