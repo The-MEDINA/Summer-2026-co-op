76cf12c75361220bfe5d2b605c204d2a27ed5ca0
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2026-07-07 10:02:33 -0600

    cards fit in the screen and can scroll now

--- Assets/Scripts/DeckbuildScripts/DeckInstanceDeckbuilderScript.cs
+++ Assets/Scripts/DeckbuildScripts/DeckInstanceDeckbuilderScript.cs
@@ -8,6 +8,10 @@ public class DeckInstanceDeckbuilderScript : MonoBehaviour
     public static DeckInstanceDeckbuilderScript instance;
 
     [SerializeField] private GameObject prefab;
+    [SerializeField] private int cardsInRow = 7;
+    [SerializeField] private float cardSpacing = 2.3f;
+    private float highYPos = -100;
+    private float lowYPos = 100;
     private List<GameObject> cardObjects = new List<GameObject>();
     private List<NewVirtualCardParent> deck = new List<NewVirtualCardParent>();
     private CommanderCardScript commander = new CommanderCardScript();
@@ -31,6 +35,30 @@ public class DeckInstanceDeckbuilderScript : MonoBehaviour
     private void Start()
     {
         ChangeFactionCards("Cat");
+    }
+
+    private void Update()
+    {
+        float move = Input.mouseScrollDelta.y;
+        if (move > 0) 
+        {
+            
+        }
+        if (move < 0)
+        {
+
+        }
+        if (move < 0 && highYPos > 3 || move > 0 && lowYPos < -3)
+        {
+            for (int i = 0; i < cardObjects.Count; i++)
+            {
+                Vector3 cardPosition = cardObjects[i].transform.position;
+                cardPosition.y += move;
+                cardObjects[i].transform.position = cardPosition;
+            }
+            highYPos += move;
+            lowYPos += move;
+        }
     }
 
     public bool AddCard(string cardName)
@@ -72,7 +100,9 @@ public class DeckInstanceDeckbuilderScript : MonoBehaviour
         for (int i = 0; i < factionCards.Count; i++)
         {
             GameObject deckCard = Instantiate(prefab);
-            deckCard.transform.position = new Vector3(-7.5f  + (2.25f * i), 3, 0);
+            deckCard.transform.position = new Vector3(-7.5f  + ((cardSpacing * i) % (cardSpacing * cardsInRow)), 3 - (2 * (i / cardsInRow)), 0);
+            if (deckCard.transform.position.y > highYPos) highYPos = deckCard.transform.position.y;
+            if (deckCard.transform.position.y < lowYPos) lowYPos = deckCard.transform.position.y;
             deckCard.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
             if (factionCards[i].type != NewVirtualCardParent.type.none)
             {
@@ -85,6 +115,7 @@ public class DeckInstanceDeckbuilderScript : MonoBehaviour
                 deckCard.GetComponent<DeckbuilderCard>().UpdateUI();
             }
             deckCard.GetComponent<DeckbuilderCard>().DeckInstance = this;
+            cardObjects.Add(deckCard);
         }
     }
 }
