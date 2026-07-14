dfa2444bf7255b4476af4fe99697e1c1376fcab2
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2026-07-14 14:51:50 -0600

    patching spell duplication breaking thru networking

--- Assets/Scripts/CardScripts/CardSelectionManager.cs
+++ Assets/Scripts/CardScripts/CardSelectionManager.cs
@@ -549,6 +549,7 @@ public class CardSelectionManager : MonoBehaviour
         SpellParent attacker = selectedCardObject.CardData as SpellParent;
         Player owner = selectedCardObject.OwnerPlayer;
         MinionParent target = targetCard.CardData as MinionParent;
+        // SpellParent targetSpell = targetCard.CardData as SpellParent;
 
         if (targetCard.CardData.CardLocation != NewVirtualCardParent.location.inPlay && attacker.Effect != SpellParent.spellEffect.copy)
         {
@@ -604,7 +605,7 @@ public class CardSelectionManager : MonoBehaviour
 
         if (!selectedCardObject.OwnerPlayer.IsPlayerTwo)
         {
-            Networking.SendCardAttack(attacker, target, false);
+            Networking.SendCardAttack(attacker, targetCard.CardData, false);
         }
 
         if (attacker.Target == SpellParent.spellTarget.allEnemies)
@@ -624,6 +625,8 @@ public class CardSelectionManager : MonoBehaviour
             attacker.OnPlay(target);
         }
 
+        Debug.Log(attacker.CardName + " played on " + targetCard.CardData.CardName);
+
         RemoveSelectedCardFromHandUI(owner);
         owner.MoveCardToDiscard(attacker);
         selectedCardObject.gameObject.SetActive(false);
@@ -631,7 +634,6 @@ public class CardSelectionManager : MonoBehaviour
         RefreshCardVisual(selectedCardObject);
         RefreshCardVisual(targetCard);
 
-        Debug.Log(attacker.CardName + " played on " + target.CardName + ". Target health: " + target.Health);
         selectedCardObject.OwnerPlayer.RegisterAction();
 
         if (!owner.IsPlayerTwo && attacker.Target == SpellParent.spellTarget.any)
