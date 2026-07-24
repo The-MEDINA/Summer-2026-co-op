6780bfd7a17e51dfd08626e059df490013b1c57c
jcf8537 <jacobfoltz428@gmail.com>
2026-07-24 10:24:04 -0400

    not anywhere near done with hivemind

---
+++ Assets/Scripts/CardScripts/CommanderCardScripts/HivemindScript.cs
@@ -0,0 +1,65 @@
+using Network;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HivemindScript : CommanderCardScript, IPointerClickHandler
+{
+    private float timer = 0f;
+    [SerializeField] private float timeToEffect = 3f;
+    private bool canAttack = false;
+
+    private void Start()
+    {
+        Name = "Seargent Zoomie";//assigns name
+    }
+
+    void Update()
+    {
+        //controls effect timer
+        if (timer > timeToEffect)
+        {
+            canAttack = true;
+            timer = 0f;
+        }
+        else if (timer < timeToEffect && !canAttack)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// calls PerformAbility() on mouse click
+    /// </summary>
+    /// <param name="eventData">data for mouse pointer click</param>
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!DeckbuilderOverride) // need this to prevent this method from activating in the deck builder
+        {
+            //when testing locally, enable bool isLocalTesting in inspector on CardSelectionManager.Ins, when playing online, disable it - Jacob
+            if (!bg.P.IsPlayerTwo || CardSelectionManager.Instance.IsLocalTesting)
+            {
+                if (canAttack) { PerformAbility(); }
+            }
+            else { Debug.LogWarning("Cannot interact with Player 2's commander."); }
+        }
+    }
+
+    /// <summary>
+    /// gives owner player +2 energy upon use
+    /// </summary>
+    public override void PerformAbility()
+    {
+        // Don't run if network manager is trying to resolve a desync.
+        if (Networking.CurrentState == state.paused) return;
+
+        //implement energy
+        //bg.P.Energy += 2;
+
+        if (!bg.P.IsPlayerTwo)
+        {
+            Networking.SendCommanderAbility(0);
+        }
+
+        canAttack = false;
+    }
+}
